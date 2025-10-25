269476ad0970990f42b044c10a19b91f3c673fe0
Vermakashish001 <kashish854104@gmail.com>
2025-10-25 21:22:54 +0530

    config: disable ESLint and TypeScript error checks during builds

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Ignore ESLint errors during builds
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Ignore TypeScript errors during builds (optional)
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
